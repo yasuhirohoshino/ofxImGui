0536a907e503849afacb5b88876726b94532b480
Yasuhiro Hoshino <yasuhiro.hoshino.0122@gmail.com>
2017-04-15 18:18:19 +0900

    fixed mouse click bug.

--- src/Gui.cpp
+++ src/Gui.cpp
@@ -144,7 +144,9 @@ namespace ofxImGui
 			io.MouseDown[i] = engine->mousePressed[i];
 
 			// Update for next frame; set to false only if the mouse has been released
-			engine->mousePressed[i] = !engine->mouseReleased;
+			if (engine->mousePressed[i]) {
+				engine->mousePressed[i] = !engine->mouseReleased;
+			}
 		}
 		ImGui::NewFrame();
 	}
